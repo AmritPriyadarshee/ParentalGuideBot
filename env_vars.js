0876a9abe590b9ee3570e7fbf951994a9fa4cf0f
Amrit <amrit.p@live.ca>
2018-07-03 00:34:21 -0400

    Delete env_vars.js

--- env_vars.js
+++
@@ -1,7 +0,0 @@
-const PAGE_ACCESS_TOKEN = "EAAcZCLToyTLwBAKNgBWvV84VujL1ZBXA8qECPNmDuLpgEVWeIZB3DltwBdn6lcxQZBXcNRJISEu2tsrVfKRZCqCkqGRxkVpdOh9e1JRsZAhzj4l8em7pVcAnkq0UTZASFPKrRvBC2WiGS33GEfnCpImet20uZAZBC9dl1ZCZCc59QHsjgZDZD"
-const password = "50.Bentoak";
-const api_key = "93a81067";
-
-exports.api_key = api_key;
-exports.password = password;
-exports.PAGE_ACCESS_TOKEN = PAGE_ACCESS_TOKEN;
